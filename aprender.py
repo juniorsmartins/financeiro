065bad1d291cceba5df3385d90554f255256ad43
juniorsmartins <junior.soares.martins@proton.me>
2024-10-05 15:04:58 -0400

    FEAT: aprendizado.

---
+++ aprender.py
@@ -0,0 +1,35 @@
+print("Aprender")
+
+
+nome = "Holga Weiss"
+idade = 12
+valor = 25.9
+verdade = True
+
+print(nome)
+print(type(nome))
+
+print(idade)
+print(type(idade))
+
+print(valor)
+print(type(valor))
+
+print(verdade)
+print(type(verdade))
+
+filme = input("Digite o nome de um filme: ")
+print("Teu filme favorito é: " + filme.capitalize())
+print("Filme favorito: ", filme)
+print(f"Filme: {filme}")
+
+ano = int(input("Ano de lançamento: "))
+print(ano)
+print(type(ano))
+
+sobrenome = input("Qual teu sobrenome: ")
+anoNascimento = int(input("Qual o ano do teu nascimento: "))
+print(f"Tua família é: {sobrenome}\n"
+    f"O ano do teu nascimento é: {anoNascimento}")
+
+
---
+++ operadores.py
@@ -0,0 +1,18 @@
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+sum = num1 + num2
+sub = num1 - num2
+div = num1 / num2
+mult = num1 * num2
+mod = num1 % num2
+exp = num1 ** num2
+
+print(f"Soma: {sum}")
+print(f"Subtração: {sub}")
+print(f"Divisão: {div}")
+print(f"Multiplicação: {mult}")
+print(f"Resto da divisão: {mod}")
+print(f"Exponenciação: {exp}")
+
+
