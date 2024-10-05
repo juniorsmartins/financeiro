05a14951bc54ee20a29fc1df9990cc07e559c353
juniorsmartins <junior.soares.martins@proton.me>
2024-10-05 16:11:34 -0400

    FEAT: strings

---
+++ slice.py
@@ -0,0 +1,21 @@
+movieName = "Top_Gun"
+
+# Buscar toda a string a partir da primeira posição
+print(movieName[0:])
+
+# Buscar toda a string a partir da posição 4
+print(movieName[4:])
+
+# Buscar toda string até a posição 6
+print(movieName[:6])
+
+# Buscar toda string de dois em dois caracteres - string[início:fim:passo]
+print(movieName[::2])
+print(movieName[0:6:2])
+
+# Buscar toda string nos índices ímpares
+print(movieName[1::2])
+
+# Inverter uma string de trás pra frente
+print(movieName[::-1])
+
