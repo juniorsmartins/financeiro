2243f97412af4fced2292a3c199f8abc7b765676
juniorsmartins <junior.soares.martins@proton.me>
2024-10-06 14:51:38 -0400

    LIST

---
+++ lista.py
@@ -0,0 +1,26 @@
+film = []
+print(type(film))
+
+film.append("Top")
+film.append("Gun")
+
+print(film)
+
+
+filmMatrix = ["Matrix", 1999, 8.7, True]
+print(filmMatrix)
+
+
+filmList = ["Inception", "Zorro", "The Dark", "Pulp Fiction"]
+
+# Buscar os dois primeiros filmes da lista
+print(filmList[:2])
+
+# Buscar o último item da lista
+print(filmList[-1])
+
+# Buscar filme do índice inicial e até anterior ao índice final
+print(filmList[1:3])
+
+
+
