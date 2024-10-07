61dd4fd70ede442a50eba33442ce84241334736d
juniorsmartins <junior.soares.martins@proton.me>
2024-10-07 17:54:15 -0400

    Tuplas

---
+++ tupla.py
@@ -0,0 +1,21 @@
+filmsTuple = ("Inception", "Zorro", "The Dark", "Pulp Fiction", "Intocáveis", "Senhor dos Anéis")
+
+print(type(filmsTuple))
+
+# Buscar os dois primeiros itens da tupla
+print(filmsTuple[:2])
+
+# Buscar último item da tupla
+print(filmsTuple[-1])
+
+# Buscar filmes até determinada posição
+print(filmsTuple[:3])
+
+# Buscar filmes de uma posição em diante
+print(filmsTuple[2:])
+
+# Recuperar o índice de um item pelo nome
+print(filmsTuple.index("Zorro"))
+
+
+
