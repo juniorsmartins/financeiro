52f280dbac9b6511154d7009dcb2f4f2050f28e5
juniorsmartins <junior.soares.martins@proton.me>
2024-10-08 18:35:49 -0400

    FEAT: configuração de settings e criação de classe abstrata padrão.

--- fintechfuture/accounting/models.py
+++ fintechfuture/accounting/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    creationDate = models.DateTimeField(auto_now_add=True)
+    updateDate = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
--- fintechfuture/fintechfuture/settings.py
+++ fintechfuture/fintechfuture/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os.path
 from pathlib import Path
+
+from django.conf.global_settings import STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounting',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
