99a328fcab2b6b3f4cc811f0b2b49e353864bc84
juniorsmartins <junior.soares.martins@proton.me>
2024-10-10 13:42:31 -0400

    Models

--- fintechfuture/accounting/models.py
+++ fintechfuture/accounting/models.py
@@ -5,28 +5,28 @@ from enum import Enum
 from django.db.models.utils import make_model_tuple
 
 class TypeOperation(Enum):
-    DEBITO = 1
-    CREDITO = 2
+    DEBIT = 1
+    CREDIT = 2
 
 class CostCenter(Enum):
-    ALIMENTACAO = 1
-    ALUGUEL = 2
-    EDUCACAO = 3
-    ELETRONICOS = 4
-    ESPORTE = 5
-    DECORACAO = 6
-    INVESTIMENTO = 7
-    LAZER = 8
+    FOOD = 1
+    RENT = 2
+    EDUCATION = 3
+    ELETRONIC = 4
+    SPORT = 5
+    HOUSE = 6
+    INVESTIMENT = 7
+    ENTERTAINMENT = 8
     PET = 9
-    PENSAO = 10
-    SALARIO = 11
-    SAUDE = 12
-    SERVICOS = 13
-    SUPLEMENTACAO = 14
-    TELEFONIA = 15
-    TRANSPORTE = 16
-    VESTUARIO = 17
-    OUTROS = 18
+    PENSION = 10
+    WAGE = 11
+    HEALTH = 12
+    SERVICES = 13
+    SUPPLEMENTATION = 14
+    TELEPHONY = 15
+    TRANSPORT = 16
+    CLOTHING = 17
+    OTHER = 18
 
 # Create your models here.
 class Base(models.Model):
@@ -37,11 +37,50 @@ class Base(models.Model):
     class Meta:
         abstract = True
 
-class Launch(Base):
+
+class CashBook(Base):
+    document = models.CharField(max_length=14, unique=True)
+    yearReference = models.DateField()
+
+    class Meta:
+        verbose_name = 'CashBook'
+        unique_together = ['document', 'yearReference']
+
+    def __str__(self):
+        return f'CashBook -> Documento: {self.document}; Ano de Referência: {self.yearReference}.'
+
+
+class DebitRegister(Base):
     description = models.CharField(max_length=250) # Descrição
-    typeOperation = models.CharField(max_length=10, choices=TypeOperation) # Tipo de Operação - Débito ou Crédito
+    typeOperation = models.CharField(max_length=10, choices=TypeOperation, default=TypeOperation.DEBIT) # Tipo de Operação - Débito ou Crédito
     amount = models.FloatField() # Valor da transação
     launchDate = models.DateField() # Data de Lançamento
-    costCenter = models.CharField(max_length=50, choices=TypeOperation) # Centro de Custo - categoria
+    costCenter = models.CharField(max_length=50, choices=CostCenter) # Centro de Custo - categoria
     supplier = models.CharField(max_length=50) # Fornecedor origem/destino
+    cashBook = models.ForeignKey(CashBook, related_name='CashBook', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "DebitRegister"
+
+    def __str__(self):
+        return self.description
+
+
+class CreditRegister(Base):
+    description = models.CharField(max_length=250) # Descrição
+    typeOperation = models.CharField(max_length=10, choices=TypeOperation, default=TypeOperation.CREDIT) # Tipo de Operação - Débito ou Crédito
+    amount = models.FloatField() # Valor da transação
+    launchDate = models.DateField() # Data de Lançamento
+    costCenter = models.CharField(max_length=50, choices=CostCenter) # Centro de Custo - categoria
+    supplier = models.CharField(max_length=50) # Fornecedor origem/destino
+    cashBook = models.ForeignKey(CashBook, related_name='CashBook', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "CreditRegister"
+
+    def __str__(self):
+        return self.description
+
+
+
 
