36283784697b6312820971f0a052b5f6f2a184e1
juniorsmartins <junior.soares.martins@proton.me>
2024-10-10 14:31:36 -0400

    ...

--- fintechfuture/accounting/admin.py
+++ fintechfuture/accounting/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import CashBook, DebitRegister, CreditRegister
+from .models import CashBookCPF, DebitRegister, CreditRegister
 
-@admin.register(CashBook)
-class CashBookAdmin(admin.ModelAdmin):
-    list_display = ('document', 'yearReference', 'created', 'updated', 'active')
+@admin.register(CashBookCPF)
+class CashBookCPFAdmin(admin.ModelAdmin):
+    list_display = ('cpf', 'yearReference', 'created', 'updated', 'active')
 
 
 @admin.register(DebitRegister)
--- fintechfuture/accounting/models.py
+++ fintechfuture/accounting/models.py
@@ -5,28 +5,38 @@ from enum import Enum
 from django.db.models.utils import make_model_tuple
 
 class TypeOperation(Enum):
-    DEBIT = 1
-    CREDIT = 2
+    DEBIT = 'DEBIT'
+    CREDIT = 'CREDIT'
+
+    @classmethod
+    def choices(cls):
+        return [(member.value, member.name) for member in cls]
+
 
 class CostCenter(Enum):
-    FOOD = 1
-    RENT = 2
-    EDUCATION = 3
-    ELETRONIC = 4
-    SPORT = 5
-    HOUSE = 6
-    INVESTIMENT = 7
-    ENTERTAINMENT = 8
-    PET = 9
-    PENSION = 10
-    WAGE = 11
-    HEALTH = 12
-    SERVICES = 13
-    SUPPLEMENTATION = 14
-    TELEPHONY = 15
-    TRANSPORT = 16
-    CLOTHING = 17
-    OTHER = 18
+    FOOD = 'FOOD'
+    RENT = 'RENT'
+    EDUCATION = 'EDUCATION'
+    ELETRONIC = 'ELETRONIC'
+    SPORT = 'SPORT'
+    HOUSE = 'HOUSE'
+    INVESTIMENT = 'INVESTIMENT'
+    ENTERTAINMENT = 'ENTERTAIMENT'
+    PET = 'PET'
+    PENSION = 'PENSION'
+    WAGE = 'WAGE'
+    HEALTH = 'HEALTH'
+    SERVICES = 'SERVICES'
+    SUPPLEMENTATION = 'SUPPLEMENTATION'
+    TELEPHONY = 'TELEPHONY'
+    TRANSPORT = 'TRANSPORT'
+    CLOTHING = 'CLOTHING'
+    OTHER = 'OTHER'
+
+    @classmethod
+    def choices(cls):
+        return [(member.value, member.name) for member in cls]
+
 
 # Create your models here.
 class Base(models.Model):
@@ -38,26 +48,26 @@ class Base(models.Model):
         abstract = True
 
 
-class CashBook(Base):
-    document = models.CharField(max_length=14, unique=True)
+class CashBookCPF(Base):
+    cpf = models.CharField(max_length=11)
     yearReference = models.DateField()
 
     class Meta:
-        verbose_name = 'CashBook'
-        unique_together = ['document', 'yearReference']
+        verbose_name = 'CashBookCPF'
+        unique_together = ['cpf', 'yearReference']
 
     def __str__(self):
-        return f'CashBook -> Documento: {self.document}; Ano de Referência: {self.yearReference}.'
+        return f'CashBookCPF -> CPF: {self.cpf}; Ano de Referência: {self.yearReference}.'
 
 
 class DebitRegister(Base):
     description = models.CharField(max_length=250) # Descrição
-    typeOperation = models.CharField(max_length=10, choices=TypeOperation, default=TypeOperation.DEBIT) # Tipo de Operação - Débito ou Crédito
+    typeOperation = models.CharField(max_length=10, choices=TypeOperation.choices, default=TypeOperation.DEBIT) # Tipo de Operação - Débito ou Crédito
     amount = models.FloatField() # Valor da transação
     launchDate = models.DateField() # Data de Lançamento
-    costCenter = models.CharField(max_length=50, choices=CostCenter) # Centro de Custo - categoria
+    costCenter = models.CharField(max_length=50, choices=CostCenter.choices) # Centro de Custo - categoria
     supplier = models.CharField(max_length=50) # Fornecedor origem/destino
-    cashBook = models.ForeignKey(CashBook, related_name='debit_cashbook', on_delete=models.CASCADE)
+    cashBook = models.ForeignKey(CashBookCPF, related_name='debit_cashbookcpf', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "DebitRegister"
@@ -68,12 +78,12 @@ class DebitRegister(Base):
 
 class CreditRegister(Base):
     description = models.CharField(max_length=250) # Descrição
-    typeOperation = models.CharField(max_length=10, choices=TypeOperation, default=TypeOperation.CREDIT) # Tipo de Operação - Débito ou Crédito
+    typeOperation = models.CharField(max_length=10, choices=TypeOperation.choices, default=TypeOperation.CREDIT) # Tipo de Operação - Débito ou Crédito
     amount = models.FloatField() # Valor da transação
     launchDate = models.DateField() # Data de Lançamento
-    costCenter = models.CharField(max_length=50, choices=CostCenter) # Centro de Custo - categoria
+    costCenter = models.CharField(max_length=50, choices=CostCenter.choices) # Centro de Custo - categoria
     supplier = models.CharField(max_length=50) # Fornecedor origem/destino
-    cashBook = models.ForeignKey(CashBook, related_name='credit_cashbook', on_delete=models.CASCADE)
+    cashBook = models.ForeignKey(CashBookCPF, related_name='credit_cashbookcpf', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "CreditRegister"
