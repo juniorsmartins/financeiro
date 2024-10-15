e906bcfd5daf68bd912d5235427dfcc146ac7da2
juniorsmartins <junior.soares.martins@proton.me>
2024-10-15 08:35:43 -0400

    Módulos

---
+++ aprendizado/modulo/main.py
@@ -0,0 +1,15 @@
+# Programa principaç
+import math_operations
+from math_operations import multiply, divide
+import string_utils
+
+print(math_operations.sum(5, 3))
+print(math_operations.subtract(5, 3))
+print(multiply(5, 3))
+print(divide(5, 2))
+
+print(string_utils.capitalize("hello"))
+print(string_utils.reverse_string("hello"))
+print(string_utils.count("hello"))
+
+
---
+++ aprendizado/modulo/math_operations.py
@@ -0,0 +1,20 @@
+# Módulo de operações matemáticas
+def sum(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        raise ValueError("Não é permitido divisão por zero.")
+
+
+
+
+
