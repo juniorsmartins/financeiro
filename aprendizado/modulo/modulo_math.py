c1519fc5d8ae3b6e5c12c7308f80cc480fe1819e
juniorsmartins <junior.soares.martins@proton.me>
2024-10-16 10:25:05 -0400

    Modulo Math

---
+++ aprendizado/modulo/modulo_math.py
@@ -0,0 +1,33 @@
+import math
+
+# Acessar o número PI
+print(math.pi)
+print(f"{math.pi:.2f}")
+
+# Acessar o número de Euler
+print(math.e)
+print(f'{math.e:.2f}')
+
+# Arredondar números para cima e para baixo
+num = 10.4
+print(math.ceil(num))
+print(math.floor(num))
+
+# Fatorial de número
+print(math.factorial(5))
+
+# Potência de números
+print(math.pow(5, 5))
+
+# Raiz quadrada
+print(math.sqrt(169))
+
+# Máximo Divisor Comum - MDC
+print(math.gcd(20, 100))
+
+# Logaritmo
+print(math.log(10))
+
+
+
+
